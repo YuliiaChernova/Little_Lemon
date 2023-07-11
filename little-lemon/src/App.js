4b514f8963c28c69412ee1a97079d7b58515d868
YuliiaChernova <yuki.chernova.judy@gmail.com>
2023-07-10 19:09:18 -0500

    HTML structure template

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -2,7 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div>Test</div>
+    <>
+      <nav>
+        <img src="" alt="Little Lemon Logo">Logo</img>
+        <span>Hamburger menu</span>
+      </nav>
+      <main>
+        <form>
+          Test form
+        </form>
+      </main>
+      <footer>
+        <img src="" alt="Little Lemon Logo">Logo</img>
+        <div>Navigation</div>
+        <div>Contact</div>
+        <div>Social Media</div>
+      </footer>
+    </>
   );
 }
 
