70961891f82ddfdb69163775b8f73087dae2694d
YuliiaChernova <yuki.chernova.judy@gmail.com>
2023-07-12 22:12:15 -0500

    Added slider

--- little-lemon/src/pages/Home.js
+++ little-lemon/src/pages/Home.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Dishes from '../components/Dishes';
 
 const Home = () => {
   return (
     <Layout>
       <Hero />
+      <Dishes />
       <About />
     </Layout>
   )
